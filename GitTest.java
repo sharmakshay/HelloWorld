fafb0d15b364396906ba761865f8072fb5d413e8
sharmakshay <sharmakshay@icloud.com>
2016-02-24 12:32:21 -0500

    Added file3.txt and made changes to other files

--- GitTest.java
+++ GitTest.java
@@ -2,6 +2,7 @@ public class GitTest{
   public static void main (String[] args){
     int x = 5;
     int y = 6;
+    int z = 10; //changes for third commit
     System.out.println("X = " + x + "Y = " + y);
   }
 }
